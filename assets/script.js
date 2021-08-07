cc44db8581318ce4aefbe604e4bd4bc8fd0e1487
Derek Lewis <derek.lewis.den@outlook.com>
2021-08-07 14:37:41 -0600

    stored time value variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,24 @@
 var currentDate = moment().format("MMM Do, YYYY"); 
 $("#currentDay").text(currentDate);
 console.log(currentDate)
+
+var currentMoment = moment().format("H");
+var nineAm = 9
+var tenAm = 10
+var elevenAm = 11
+var noon = 12
+var onePm = 13
+var twoPm = 14
+var threePm = 15
+var fourPm = 16
+var fivePm = 17
+
+var calenarTimes = []
+console.log(currentMoment)
+if(currentMoment > nineAm) {
+    console.log("It's past")
+}
+else {
+    console.log("It's wrong")
+}
+//create blocks of time that change color based on the current time and can be written into and saved in local storage
