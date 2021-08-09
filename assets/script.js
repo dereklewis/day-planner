506f237f25f1fb4708078c194d449df1a8d28f30
Derek Lewis <derek.lewis.den@outlook.com>
2021-08-09 07:56:58 -0600

    another attempt at getting the rows to change colors

--- assets/script.js
+++ assets/script.js
@@ -3,22 +3,53 @@ $("#currentDay").text(currentDate);
 console.log(currentDate)
 
 var currentMoment = moment().format("H");
-var nineAm = 9
-var tenAm = 10
-var elevenAm = 11
-var noon = 12
-var onePm = 13
-var twoPm = 14
-var threePm = 15
-var fourPm = 16
-var fivePm = 17
 
-var calenarTimes = []
-console.log(currentMoment)
-if(currentMoment > nineAm) {
-    console.log("It's past")
+
+var fourPm = document.getElementById("four-pm");
+var threePm = document.getElementById("three-pm");
+
+var allTimeblocks = ["fivePm", "fourPm", "threePm"]
+
+if (fivePm < currentMoment) {
+    .setAttribute("class", "past");
+}
+
+
+
+function pastPresentFuture() {
+
+    for(i=0;i < allTimeblocks.length; i++) {
+        if (allTimeblocks[0] < currentMoment) {
+            allTimeblocks.setAttribute("class", "past");
+        }
+        // else if (allTimeblocks = currentMoment) {
+        //      allTimeblocks.setAttribute("class", "present");
+        // }
+        // else {
+        //      allTimeblocks.setAttribute("class", "future");
+        // }
+    }
+    pastPresentFuture()
+}
+
+
+
+
+// function calendarTimes() {
+
+if(currentMoment > fourPm) {
+   var element = document.getElementById("fourPm");
+   fourPm.classList.toggle("past");
+}
+else if (currentMoment === fourPm) {
+    
+   fourPm.classList.toggle("present");
 }
 else {
-    console.log("It's wrong")
+    
+   fourPm.classList.toggle("future");
 }
+
+// }
+// calendarTimes()
 //create blocks of time that change color based on the current time and can be written into and saved in local storage
